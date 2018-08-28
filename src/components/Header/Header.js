af4ff61190b2c173f9a87e913d76be5339ab77bd
Sunny-zz <b695340402@163.com>
2018-08-28 11:24:55 +0800

    first

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React shopping-cart</h1>
+        <span>2112</span>
+      </div>
+    )
+  }
+}
+
+export default Header
